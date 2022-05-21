7ff0a7aeaed952314a9da39ff02a659de0ac8112
siyu0927 <68551758+siyu0927@users.noreply.github.com>
2022-05-21 12:25:18 +0800

    Create 3.js

---
+++ homework/JavaScript/3.js
@@ -0,0 +1,14 @@
+//質數
+function a(n)
+{
+    var i,j=0;
+    for(i=2;i<n;i++)
+    {
+        if(n%i==0) j=1;
+        
+    }
+    if(j==0) return true;
+    if(j==1) return false;
+}
+
+console.log(a(13));
